a4f0ecd6cf6603bfad99ccd58886baa579e1b77b
Electrical5 <xfuaze@gmail.com>
2025-06-02 22:51:57 +0200

    Initial commit
    Copied the example off AdiIRC/APIv2 github

---
+++ AdiIRCPlugin.cs
@@ -0,0 +1,26 @@
+﻿using AdiIRCAPIv2.Interfaces;
+
+namespace AdiIRC_LibreTranslate_plugin
+{
+    public class AdiIRC_LibreTranslate_plugin : IPlugin
+    {
+        public string PluginName => "AdiIRC LibreTranslate Plugin";
+        public string PluginDescription => "Translates chat messages";
+        public string PluginAuthor => "";
+        public string PluginVersion => "0.1";
+        public string PluginEmail => "";
+
+        private IPluginHost _host;
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize(IPluginHost pluginHost)
+        {
+            _host = pluginHost;
+            var activeWindow = _host.ActiveIWindow;
+            activeWindow.OutputText("LibreTranslate plugin loaded.");
+        }
+    }
+}
